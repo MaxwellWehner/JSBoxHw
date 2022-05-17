e9e678d1b7b1cb862ca6c0cc39b9ee05a1835139
Max Wehner <maxwehnerwork@gmail.com>
2022-05-17 12:32:37 -0700

    finished problem a

--- a.js
+++ a.js
@@ -1,1 +1,10 @@
-console.log("program start");
+let input = prompt("Enter a height and width");
+let inputArr = input.split(" ").map((x) => Number.parseInt(x));
+let str = "";
+for (let i = 0; i < inputArr[0]; i++) {
+	for (let j = 0; j < inputArr[1]; j++) {
+		str += "*";
+	}
+	str += "\n";
+}
+console.log(str);
